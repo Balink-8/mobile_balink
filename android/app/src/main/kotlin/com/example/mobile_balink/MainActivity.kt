2508c7cead4c24aab7ee819f38639e207ee524d4
azrilnrr <130768231+azrilnrr@users.noreply.github.com>
2023-05-25 19:52:57 +0700

    setup awal

---
+++ android/app/src/main/kotlin/com/example/mobile_balink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_balink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xfffef7ee);
+Color secondaryColor = const Color(0xff5C5045);
+Color thirdColor = const Color(0xffC2813E);
+Color whiteColor = Colors.white;
+Color blackColor = Colors.black;
+
+TextStyle poppinsKecil =
+    GoogleFonts.poppins(fontSize: 14.sp, color: whiteColor);
