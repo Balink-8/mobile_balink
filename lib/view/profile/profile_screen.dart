53bf9219932a09c96545c16931f25e6082c4c24b
Mohd Ryan Obillah <ryanobillah@gmail.com>
2023-05-27 01:04:59 +0700

    fix container size buildTransaksiAtikel and add pick file image feature

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -31,223 +31,222 @@ class ProfileScreen extends StatelessWidget {
 
   GestureDetector buildImageUser(BuildContext context) {
     return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return const DetailProfileScreen();
-                },
-              ),
-            );
-          },
-          child: Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundImage: const AssetImage('assets/profil.png'),
-                  radius: 60.r,
-                ),
-                SizedBox(
-                  height: 2.h,
-                ),
-                Text(
-                  'User',
-                  style: GoogleFonts.poppins(
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const DetailProfileScreen();
+            },
           ),
         );
+      },
+      child: Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: const AssetImage('assets/profil.png'),
+              radius: 60.r,
+            ),
+            SizedBox(
+              height: 2.h,
+            ),
+            Text(
+              'User',
+              style: GoogleFonts.poppins(
+                fontSize: 24.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Padding buildIdentitasUser() {
     return Padding(
-          padding: const EdgeInsets.all(16),
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.r),
-              border: Border.all(
-                color: Colors.black,
-              ),
-            ),
-            child: Column(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.r),
+          border: Border.all(
+            color: Colors.black,
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Email',
-                      style: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'user@example.com',
-                          style: GoogleFonts.poppins(
-                            fontSize: 12.sp,
-                          ),
-                        ),
-                        SizedBox(width: 4.w),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 15.w,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const Divider(
-                  thickness: 2,
+                Text(
+                  'Email',
+                  style: GoogleFonts.poppins(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'No. Hp',
+                      'user@example.com',
                       style: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 12.sp,
                       ),
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          '0823 1234 5678',
-                          style: GoogleFonts.poppins(
-                            fontSize: 12.sp,
-                          ),
-                        ),
-                        SizedBox(width: 4.w),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 15.w,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const Divider(
-                  thickness: 2,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Alamat',
-                      style: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'Maluku',
-                          style: GoogleFonts.poppins(
-                            fontSize: 12.sp,
-                          ),
-                        ),
-                        SizedBox(width: 4.w),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 15.w,
-                        ),
-                      ],
+                    SizedBox(width: 4.w),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 15.w,
                     ),
                   ],
                 ),
               ],
             ),
-          ),
-        );
+            const Divider(
+              thickness: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'No. Hp',
+                  style: GoogleFonts.poppins(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '0823 1234 5678',
+                      style: GoogleFonts.poppins(
+                        fontSize: 12.sp,
+                      ),
+                    ),
+                    SizedBox(width: 4.w),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 15.w,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Alamat',
+                  style: GoogleFonts.poppins(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Maluku',
+                      style: GoogleFonts.poppins(
+                        fontSize: 12.sp,
+                      ),
+                    ),
+                    SizedBox(width: 4.w),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 15.w,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Padding buildTransaksiArtikel() {
     return Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 15,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.r),
-                  border: Border.all(
-                    color: Colors.black.withOpacity(0.3),
-                  ),
-                ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: 40.w,
-                        child: Image.asset(
-                          'assets/icon/setting_icon/receipt_long.png',
-                        ),
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        'Transaksi',
-                        style: GoogleFonts.poppins(
-                          fontSize: 18.sp,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 120.h,
+            width: 160.w,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.3),
               ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 60,
-                  vertical: 20,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.r),
-                  border: Border.all(
-                    color: Colors.black.withOpacity(0.3),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image.asset(
+                      'assets/icon/setting_icon/receipt_long.png',
+                      scale: 2.r,
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: 40.w,
-                        child: Image.asset(
-                          'assets/icon/setting_icon/newspaper.png',
-                        ),
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        'Artikel',
-                        style: GoogleFonts.poppins(
-                          fontSize: 18.sp,
-                        ),
-                      ),
-                    ],
+                  SizedBox(height: 8.h),
+                  Text(
+                    'Transaksi',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18.sp,
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        );
+          Container(
+            height: 120.h,
+            width: 160.w,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.3),
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    // width: 40.w,
+                    child: Image.asset(
+                      'assets/icon/setting_icon/newspaper.png',
+                      scale: 2.r,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Text(
+                    'Artikel',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Padding buildSetting() {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: EdgeInsets.all(16.r),
       child: Container(
-        padding: const EdgeInsets.all(12),
+        padding: EdgeInsets.all(12.r),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.r),
           border: Border.all(
