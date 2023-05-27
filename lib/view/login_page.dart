c756edc3dbfdcb284f42a54908b0fcc61477171a
azrilnrr <130768231+azrilnrr@users.noreply.github.com>
2023-05-27 23:43:17 +0700

    update

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -13,6 +13,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool showPassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,35 +27,15 @@ class _LoginScreenState extends State<LoginScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                height: 130.h,
-                width: 400.w,
-                color: secondaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'Login dan nikmati fitur yang kami sediakan!',
-                          maxLines: 2,
-                          style: poppinsKecil.copyWith(
-                              fontSize: 16.sp, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      CircleAvatar(
-                        backgroundColor: primaryColor,
-                        radius: 40.r,
-                        child: CircleAvatar(
-                          backgroundColor: primaryColor,
-                          radius: 30.r,
-                          child: Image.asset('assets/logo.png'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 40.h),
+                  height: 130.h,
+                  width: 400.w,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/logo.png',
+                      width: 70.w,
+                    ),
+                  )),
+              SizedBox(height: 10.h),
               Padding(
                 padding: const EdgeInsets.only(
                   left: 30,
@@ -60,17 +45,33 @@ class _LoginScreenState extends State<LoginScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Email atau Username',
+                      'Email / Username',
                       style: poppinsKecil.copyWith(
                           fontSize: 12.sp, color: blackColor),
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Username@gmail.com',
-                        prefixIcon: Icon(Icons.person),
-                        border: OutlineInputBorder(),
+                    Container(
+                      width: 320.w,
+                      height: 48.h,
+                      decoration: BoxDecoration(
+                          color: whiteColor,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.w))),
+                      child: TextFormField(
+                        controller: usernameController,
+                        decoration: InputDecoration(
+                          hintText: 'Masukkan Email / Username',
+                          hintStyle: poppinsKecil.copyWith(
+                              color: Colors.grey, fontSize: 12.sp),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.w)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: secondaryColor,
+                          )),
+                        ),
                       ),
-                      style: poppinsKecil,
                     ),
                     SizedBox(height: 12.h),
                     Text(
@@ -78,35 +79,49 @@ class _LoginScreenState extends State<LoginScreen> {
                       style: poppinsKecil.copyWith(
                           fontSize: 12.sp, color: blackColor),
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Username@gmail.com',
-                        prefixIcon: Icon(Icons.lock),
-                        border: OutlineInputBorder(),
+                    Container(
+                      width: 320.w,
+                      height: 48.h,
+                      decoration: BoxDecoration(
+                          color: whiteColor,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.w))),
+                      child: TextFormField(
+                        controller: passwordController,
+                        obscureText: showPassword,
+                        decoration: InputDecoration(
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                if (showPassword) {
+                                  setState(() {
+                                    showPassword = false;
+                                  });
+                                } else {
+                                  setState(() {
+                                    showPassword = true;
+                                  });
+                                }
+                              },
+                              child: showPassword
+                                  ? Icon(Icons.visibility,
+                                      color: secondaryColor)
+                                  : Icon(Icons.visibility_off,
+                                      color: secondaryColor)),
+                          hintText: 'Masukkan Password',
+                          hintStyle: poppinsKecil.copyWith(
+                              color: Colors.grey, fontSize: 12.sp),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.w)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: secondaryColor,
+                          )),
+                        ),
                       ),
-                      style: poppinsKecil,
                     ),
                     SizedBox(height: 30.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        LupaPasswordScreen()));
-                          },
-                          child: Text(
-                            'Lupa Password ?',
-                            style: poppinsKecil.copyWith(
-                                fontSize: 12.sp, color: secondaryColor),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20.h),
                     GestureDetector(
                       onTap: () {
                         Navigator.push(
@@ -123,13 +138,40 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                         child: Center(
                           child: Text(
-                            'Sign In',
-                            style: poppinsKecil.copyWith(),
+                            'LOG IN',
+                            style: poppinsKecil.copyWith(
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
                     ),
                     SizedBox(height: 40.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Lupa Password? ',
+                          style: poppinsKecil.copyWith(
+                              fontSize: 12.sp, color: secondaryColor),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        LupaPasswordScreen()));
+                          },
+                          child: Text(
+                            'Reset',
+                            style: poppinsKecil.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: secondaryColor),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.h),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -146,7 +188,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                     builder: (context) => RegisterScreen()));
                           },
                           child: Text(
-                            'Sign Up',
+                            'Registrasi',
                             style: poppinsKecil.copyWith(
                                 fontWeight: FontWeight.bold,
                                 color: secondaryColor),
