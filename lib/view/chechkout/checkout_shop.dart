0a4ec8afb97b73aba705842a97ec2ee2ba6b3041
Anggundini17 <anggundiniwahyuni@gmail.com>
2023-05-29 18:08:56 +0700

    Publish New Checkout Shop Feature Branch

---
+++ lib/view/chechkout/checkout_shop.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_balink/config/theme.dart';
+
+class CheckoutShopPage extends StatelessWidget {
+  const CheckoutShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Checkout',
+        ),
+      ),
+    );
+  }
+}
