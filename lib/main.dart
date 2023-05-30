3b6619b817fcce7bf073e2411eeb68b8ff8c9e87
Mohd Ryan Obillah <ryanobillah@gmail.com>
2023-05-31 00:22:06 +0700

    fix profile screen, photo picker, edit profile and connect to pengaturan screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mobile_balink/view/profile/pengaturan_screen.dart';
 import 'package:mobile_balink/view/profile/profile_screen.dart';
 
 void main() {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         return const MaterialApp(
           title: 'Balink',
           debugShowCheckedModeBanner: false,
-          home: PengaturanScreen(),
+          home: ProfileScreen(),
         );
       },
     );
