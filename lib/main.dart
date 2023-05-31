8ee132a2e775f8df69d39565b083053cebc529f5
Novita Siti Julaeha <novitasitijulaeha03@gmail.com>
2023-05-31 20:58:24 +0700

    shopping page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'view/shopping/sopping_page/shop_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         );
       },
       // child: LoginScreen(),
+      child: const ShopScreen(),
     );
   }
 }
