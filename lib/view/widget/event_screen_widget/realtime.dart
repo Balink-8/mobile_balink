fa422de9183e3e23b809783a7dfcc35457497171
Anggundini17 <anggundiniwahyuni@gmail.com>
2023-06-21 18:56:50 +0700

    fixed bug event

--- lib/view/widget/event_screen_widget/realtime.dart
+++ lib/view/widget/event_screen_widget/realtime.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
@@ -12,6 +14,23 @@ class RealTimeScreen extends StatefulWidget {
 }
 
 class _RealTimeScreentate extends State<RealTimeScreen> {
+  // String currentTime = '';
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getCurrentTime();
+  // }
+
+  // void getCurrentTime() {
+  //   String formattedTime = DateFormat('HH:mm:ss').format(DateTime.now());
+  //   setState(() {
+  //     currentTime = formattedTime;
+  //   });
+  //   // Update jam setiap detik
+  //   Future.delayed(Duration(seconds: 1), getCurrentTime);
+  // }
+
   String currentTime = '';
 
   @override
@@ -20,13 +39,23 @@ class _RealTimeScreentate extends State<RealTimeScreen> {
     getCurrentTime();
   }
 
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed
+    super.dispose();
+    _timer.cancel();
+  }
+
+  late Timer _timer;
+
   void getCurrentTime() {
     String formattedTime = DateFormat('HH:mm:ss').format(DateTime.now());
     setState(() {
       currentTime = formattedTime;
     });
-    // Update jam setiap detik
-    Future.delayed(Duration(seconds: 1), getCurrentTime);
+
+    // Update the time every second
+    _timer = Timer(Duration(seconds: 1), getCurrentTime);
   }
 
   @override
@@ -47,6 +76,23 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  // String currentDate = '';
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getCurrentDate();
+  // }
+
+  // void getCurrentDate() {
+  //   String formattedDate = DateFormat('EEEE, MMMM d, y').format(DateTime.now());
+  //   setState(() {
+  //     currentDate = formattedDate;
+  //   });
+
+  //   Future.delayed(Duration(seconds: 1), getCurrentDate);
+  // }
+
   String currentDate = '';
 
   @override
@@ -55,13 +101,23 @@ class _CalendarState extends State<Calendar> {
     getCurrentDate();
   }
 
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed
+    super.dispose();
+    _timer.cancel();
+  }
+
+  late Timer _timer;
+
   void getCurrentDate() {
     String formattedDate = DateFormat('EEEE, MMMM d, y').format(DateTime.now());
     setState(() {
       currentDate = formattedDate;
     });
 
-    Future.delayed(Duration(seconds: 1), getCurrentDate);
+    // Update the date every second
+    _timer = Timer(Duration(seconds: 1), getCurrentDate);
   }
 
   @override
