4273eff3ada01e1b23da90d4f45b7cc110348736
Mutiara Sabrina <mutiarajauhari1910@gmail.com>
2023-06-25 16:44:01 +0700

    lagii clock

--- lib/view/widget/event_screen_widget/card_clock.dart
+++ lib/view/widget/event_screen_widget/card_clock.dart
@@ -11,7 +11,7 @@ class cardClock extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 150.w,
-      height: 130.h,
+      height: 140.h,
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: Padding(
